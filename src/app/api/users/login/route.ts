1166e96d910897341a17e649033d6f0ffad19918
Ravish Bisht <ravishbisht85@gmail.com>
2024-05-31 16:54:41 +0530

    login api add

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,63 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function POST(request: NextRequest) {
+  await connect();
+  try {
+    const { email, password } = await request.json();
+
+    //check user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "user does not exists",
+        },
+        { status: 404 }
+      );
+    }
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "invalid credentials",
+        },
+        { status: 400 }
+      );
+    }
+    //create token data
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    //creating token token
+    const token = await jwt.sign(tokenData, process.env.JWT_TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json(
+      {
+        success: true,
+        message: "Login successfully",
+      },
+      { status: 200 }
+    );
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: error.message || " something went wrong in login route",
+      },
+      { status: 500 }
+    );
+  }
+}
--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,7 +2,6 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { ApiResponse } from "@/types/ApiResponse";
 
 export async function POST(request: NextRequest): Promise<any> {
   await connect();
