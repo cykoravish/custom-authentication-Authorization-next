1337dc185d242b217bf9cfeab505c17a6f3424d9
Ravish Bisht <ravishbisht85@gmail.com>
2024-06-01 07:41:10 +0530

    logout functionality integrate with frontend

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+"use client";
 
-export default function page() {
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+export default function LogoutPage() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success(response.data.message);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("error in logout: ", error);
+      toast.error(error.response.data.message);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile here</p>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
     </div>
   );
 }
